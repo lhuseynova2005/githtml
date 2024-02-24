e6c9416904df96eaa6fea194b34d341c8852e90c
LalaHuseynova <lhuseynova18047@ada.edu.az>
2024-02-24 22:02:10 +0400

    Main5 is created

--- Main5.java
+++ Main5.java
@@ -1,5 +1,5 @@
-public class Main {
-    public class void main (Strings []args) {
+public class Main5 {
+       public class void main (Strings []args) {
         System.out.println("Hello world");
          System.out.println("Hello");
          int sum=8;
