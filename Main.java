25473c12d856b0406e60947d005ce408915d0574
LalaHuseynova <lhuseynova18047@ada.edu.az>
2024-02-24 21:55:15 +0400

    new branch: add sum operations and h3 in html

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
     public class void main (Strings []args) {
         System.out.println("Hello world");
          System.out.println("Hello");
+         int sum=8;
+         System.out.println(sum);
 
     }
 }
